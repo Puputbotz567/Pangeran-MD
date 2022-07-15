e36757b1981b3ad0dedc5781baf3fe3a23dc7e0c
Puputbotz567 <109228837+Puputbotz567@users.noreply.github.com>
2022-07-15 22:21:39 +0700

    Update main-sewa.js

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -10,9 +10,9 @@ let anu = `Hallo mypren 👋
 ━━━━━━━━━━━━━━━━━━━━
 ━━━〔 ıll *HARGA* llı 〕━━㉿
 ⬡ *1 BULAN* 5.000
-⬡ *PERMANEN 2 grup:* 10.000
-⬡ *PERMANEN 3-6 grup:* 20.000
-⟩» lebih dari 6 tambah +5.000
+⬡ *1 TAHUN 2 grup:* 10.000
+⬡ *1 TAHUN 4 grup:* 20.000
+⟩» lebih dari 4 tambah +5.000
 📮 *batas scan 5 kali !*
 ┗━━━━━━━━━━━━━━━━━━㉿
 ┏━━━〔 ıll *PAYMENT* llı 〕━━㉿
